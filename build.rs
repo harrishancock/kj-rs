6d42ef95764c1f08e437b76199266c0b9731c91f
Harris Hancock <harris@cloudflare.com>
2025-02-26 17:14:34 +0000

    Add license
    Since I originally developed this in workerd, it's using the workerd license.

--- build.rs
+++ build.rs
@@ -1,3 +1,7 @@
+// Copyright (c) 2024 Cloudflare, Inc.
+// Licensed under the Apache 2.0 license found in the LICENSE file or at:
+//     https://opensource.org/licenses/Apache-2.0
+
 use cxx_build::CFG;
 
 use std::env;
